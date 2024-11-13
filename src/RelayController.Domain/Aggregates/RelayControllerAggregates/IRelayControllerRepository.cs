0a86383e4679ba380219b2b271561d8533ccdad1
ogabrielinacio <gabrieldev@disroot.org>
2024-11-13 14:01:30 -0300

    fix: change repository interface parameters to controller

--- src/RelayController.Domain/Aggregates/RelayControllerAggregates/IRelayControllerRepository.cs
+++ src/RelayController.Domain/Aggregates/RelayControllerAggregates/IRelayControllerRepository.cs
@@ -3,8 +3,8 @@
 public interface IRelayControllerRepository
 {
     Task<RelayController?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
-    Task AddAsync(RelayController boiler, CancellationToken cancellationToken);
-    void Update(RelayController boiler, CancellationToken cancellationToken);
+    Task AddAsync(RelayController controller, CancellationToken cancellationToken);
+    void Update(RelayController controller, CancellationToken cancellationToken);
     Task DeleteAsync(Guid id, CancellationToken cancellationToken);
     Task<IEnumerable<RelayController>> GetAllActiveAsync(CancellationToken cancellationToken);
 }
