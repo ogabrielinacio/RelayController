759249a6ed48fc99037441da2a6cdc2f88dbde0b
ogabrielinacio <gabrieldev@disroot.org>
2025-06-22 16:26:21 -0300

    fix: handle message ack/nack correctly in async consumer

--- src/RelayController.Infrastructure/Messaging/RabbitMqService.cs
+++ src/RelayController.Infrastructure/Messaging/RabbitMqService.cs
@@ -37,22 +37,34 @@ public class RabbitMqService : IMessageBusService
     {
         var connection = await _rabbitMqConnection.GetConnection();
         
-        _channel ??= await connection.CreateChannelAsync();
+        _channel ??= await connection.CreateChannelAsync(cancellationToken: cancellationToken);
 
-        var queueName = await _channel.QueueDeclareAsync(queue:"dotnet-subscriber" ,exclusive: false, durable: false, autoDelete: false, arguments: null);
+        var queueName = await _channel.QueueDeclareAsync(queue:"dotnet-subscriber" ,exclusive: false, durable: false, autoDelete: false, arguments: null, cancellationToken: cancellationToken);
         
-        await _channel.QueueBindAsync(queue: queueName, exchange: _exchangeName, routingKey: routingKey);
+        await _channel.QueueBindAsync(queue: queueName, exchange: _exchangeName, routingKey: routingKey, cancellationToken: cancellationToken);
         
-        await _channel.QueuePurgeAsync(queueName);
+        await _channel.QueuePurgeAsync(queueName, cancellationToken);
 
         var consumer = new AsyncEventingBasicConsumer(_channel);
         consumer.ReceivedAsync  += async (sender, eventsArgs) =>
         {
             var body = eventsArgs.Body.ToArray();
             var message = Encoding.UTF8.GetString(body);
+            
+            try
+            {
+                await onMessageReceived(message);
+                await _channel.BasicAckAsync(eventsArgs.DeliveryTag, multiple: false, cancellationToken: cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Message Error Rabbitmq: {ex.Message}");
+                await _channel.BasicNackAsync(eventsArgs.DeliveryTag, multiple: false, requeue: false, cancellationToken: cancellationToken);
+            }
+            
             await onMessageReceived(message);
         };
 
-        await _channel.BasicConsumeAsync(queue: queueName, autoAck: false, consumer: consumer);
+        await _channel.BasicConsumeAsync(queue: queueName, autoAck: false, consumer: consumer, cancellationToken: cancellationToken);
     }
 }
